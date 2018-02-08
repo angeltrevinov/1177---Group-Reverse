1c96d63aa3cb84d7f1f68c74e12538e4555231da
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-02-08 14:19:42 -0600

    adding main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+  Angel Odiel Treviño Villanueva
+  A01336559
+  1177 - Group Reverse  Fecha: 8/feb Materia: TopTec
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  string completo;
+  while(getline(cin, completo)){
+
+  }
+
+  return 0;
+}
